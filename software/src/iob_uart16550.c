004c6d4225a94d0bf2fee53a96b4bf3021414391
Andre Nobrega <29956896+And-Nob@users.noreply.github.com>
2025-01-14 22:57:07 +0000

    Renamed iob-uart16550 to iob_uart16550

--- software/src/iob_uart16550.c
+++ software/src/iob_uart16550.c
@@ -1,46 +1,49 @@
 #include <stdint.h>
-#include "iob-uart16550.h"
+#include "iob_uart16550.h"
+#include "iob_printf.h"
 
 static int base;
 
-//TX FUNCTIONS
+// TX FUNCTIONS
 void uart16550_txwait() {
-    while(!uart16550_txready());
+  while (!uart16550_txready())
+    ;
 }
 
 char uart16550_txready() {
-    uint8_t status = 0;
-    status = *((volatile uint8_t *)(base + 5));
-    return (status & (0x01<<6));
+  uint8_t status = 0;
+  status = *((volatile uint8_t *)(base + 5));
+  return (status & (0x01 << 6));
 }
 
 void uart16550_putc(char c) {
-    uart16550_txwait();
-    *((volatile uint8_t *)(base)) = c;
+  uart16550_txwait();
+  *((volatile uint8_t *)(base)) = c;
 }
 
-//RX FUNCTIONS
+// RX FUNCTIONS
 void uart16550_rxwait() {
-    while(!uart16550_rxready());
+  while (!uart16550_rxready())
+    ;
 }
 
 char uart16550_rxready() {
-    uint8_t status = 0;
-    status = *((volatile uint8_t *)(base + 5));
-    return (status & (0x01));
+  uint8_t status = 0;
+  status = *((volatile uint8_t *)(base + 5));
+  return (status & (0x01));
 }
 
 char uart16550_getc() {
-    uint8_t rvalue;
-    uart16550_rxwait();
-    rvalue = *((volatile uint8_t *)(base));
-    return rvalue;
+  uint8_t rvalue;
+  uart16550_rxwait();
+  rvalue = *((volatile uint8_t *)(base));
+  return rvalue;
 }
 
-//UART basic functions
+// UART basic functions
 void uart16550_init(int base_address, uint16_t div) {
-  //capture base address for good
-	base = base_address;
+  // capture base address for good
+  base = base_address;
 
   // Set the Line Control Register to the desired line control parameters.
   // Set bit 7 to ‘1’ to allow access to the Divisor Latches.
@@ -51,28 +54,29 @@ void uart16550_init(int base_address, uint16_t div) {
 
   // Set the Divisor Latches, MSB first, LSB next.
   uint8_t *dl = (uint8_t *)&div;
-  *((volatile uint8_t *)(base + 1)) = *(dl+1);
+  *((volatile uint8_t *)(base + 1)) = *(dl + 1);
   *((volatile uint8_t *)(base)) = *(dl);
 
   // Set bit 7 of LCR to ‘0’ to disable access to Divisor Latches.
-  // At this time the transmission engine starts working and data can be sent and received.
+  // At this time the transmission engine starts working and data can be sent
+  // and received.
   lcr = (lcr & 0x7F);
   *((volatile uint8_t *)(base + 3)) = lcr;
 
-  // Set the FIFO trigger level. Generally, higher trigger level values produce less
-  // interrupt to the system, so setting it to 14 bytes is recommended if the system
-  // responds fast enough.
+  // Set the FIFO trigger level. Generally, higher trigger level values produce
+  // less interrupt to the system, so setting it to 14 bytes is recommended if
+  // the system responds fast enough.
   *((volatile uint8_t *)(base + 2)) = 0xC0;
 
-  // Enable desired interrupts by setting appropriate bits in the Interrupt Enable
-  // register.
+  // Enable desired interrupts by setting appropriate bits in the Interrupt
+  // Enable register.
   *((volatile uint8_t *)(base + 1)) = 0x03;
 }
 
 // Change UART base
 int uart16550_base(int base_address) {
   int previous = base;
-	base = base_address;
+  base = base_address;
   return previous;
 }
 
@@ -81,54 +85,55 @@ void uart16550_finish() {
   uart16550_txwait();
 }
 
-//Print string, excluding end of string (0)
+// Print string, excluding end of string (0)
 void uart16550_puts(const char *s) {
-  while (*s) uart16550_putc(*s++);
+  while (*s)
+    uart16550_putc(*s++);
 }
 
-//Sends the name of the file to use, including end of string (0)
-void uart16550_sendstr (char* name) {
-  int i=0;
+// Sends the name of the file to use, including end of string (0)
+void uart16550_sendstr(char *name) {
+  int i = 0;
   do
     uart16550_putc(name[i]);
   while (name[i++]);
 }
 
-//Receives file into mem
-int uart16550_recvfile(char* file_name, char *mem) {
+// Receives file into mem
+int uart16550_recvfile(char *file_name, char *mem) {
 
   uart16550_puts(UART_PROGNAME);
-  uart16550_puts (": requesting to receive file\n");
+  uart16550_puts(": requesting to receive file\n");
 
-  //send file receive request
-  uart16550_putc (FRX);
+  // send file receive request
+  uart16550_putc(FRX);
 
-  //clear input buffer
-  while(uart16550_rxready()) uart16550_getc();
+  // clear input buffer
+  while (uart16550_rxready())
+    uart16550_getc();
 
-  //send file name
+  // send file name
   uart16550_sendstr(file_name);
 
+  // receive file size
+  int file_size = (unsigned int)uart16550_getc();
+  file_size |= ((unsigned int)uart16550_getc()) << 8;
+  file_size |= ((unsigned int)uart16550_getc()) << 16;
+  file_size |= ((unsigned int)uart16550_getc()) << 24;
 
-  //receive file size
-  int file_size = (unsigned int) uart16550_getc();
-  file_size |= ((unsigned int) uart16550_getc()) << 8;
-  file_size |= ((unsigned int) uart16550_getc()) << 16;
-  file_size |= ((unsigned int) uart16550_getc()) << 24;
-
-  //allocate space for file if file pointer not initialized
-  if(mem == NULL) {
-    mem = (char *) malloc(file_size);
+  // allocate space for file if file pointer not initialized
+  if (mem == NULL) {
+    mem = (char *)malloc(file_size);
     if (mem == NULL) {
       uart16550_puts(UART_PROGNAME);
       uart16550_puts("Error: malloc failed");
     }
   }
 
-  //send ACK before receiving file
+  // send ACK before receiving file
   uart16550_putc(ACK);
 
-  //write file to memory
+  // write file to memory
   for (int i = 0; i < file_size; i++) {
     mem[i] = uart16550_getc();
   }
@@ -139,16 +144,16 @@ int uart16550_recvfile(char* file_name, char *mem) {
   return file_size;
 }
 
-//Sends mem contents to a file
+// Sends mem contents to a file
 void uart16550_sendfile(char *file_name, int file_size, char *mem) {
 
   uart16550_puts(UART_PROGNAME);
   uart16550_puts(": requesting to send file\n");
 
-  //send file transmit command
+  // send file transmit command
   uart16550_putc(FTX);
 
-  //send file name
+  // send file name
   uart16550_sendstr(file_name);
 
   // send file size
